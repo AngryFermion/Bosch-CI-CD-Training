acce88d620420b7b8663e0c64104f9bea87d341d
Vishnu H Unnithan <106134771+AngryFermion@users.noreply.github.com>
2026-03-12 13:28:54 +0530

    removed add.py content

--- Demo1-Continuous Integration/add.py
+++ Demo1-Continuous Integration/add.py
@@ -1,12 +1,1 @@
-def add(a, b):
-    """
-    Add two numbers together.
 
-    Args:
-        a: First number
-        b: Second number
-
-    Returns:
-        The sum of a and b
-    """
-    return a + b
